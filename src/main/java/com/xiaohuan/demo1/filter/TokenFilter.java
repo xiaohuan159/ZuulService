c60013563ce398c01fc60e7b6125cdc1ad59e257
xiaohuan159 <jiaqiong1591@163.com>
2019-12-18 00:56:41 +0800

    ..

---
+++ src/main/java/com/xiaohuan/demo1/filter/TokenFilter.java
@@ -0,0 +1,34 @@
+package com.xiaohuan.demo1.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class TokenFilter extends ZuulFilter{
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        String token = request.getParameter("token");
+
+        return null;
+    }
+}
---
+++ src/test/java/com/xiaohuan/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohuan.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
